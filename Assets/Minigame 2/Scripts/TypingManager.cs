71bfc2909d7fe4d8ade06ee477aca12759725295
yasersyafa <89734955+yasersyafa@users.noreply.github.com>
2024-10-11 13:59:53 +0700

    create feature: implement logic and ui minigame

---
+++ Assets/Minigame 2/Scripts/TypingManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TypingManager : MonoBehaviour
+{
+    
+    private string currentWord;
+    public TextMeshProUGUI text;
+    private WordBank wordBank;
+    GameManager manager;
+
+    void Awake() {
+        manager = GameManager.instance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        wordBank = GetComponent<WordBank>();
+        currentWord = wordBank.GetTargetWord();
+        text.text = currentWord;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Timer.instance.isFinished)
+        {
+            string inputChar = Input.inputString;
+
+            if (!string.IsNullOrEmpty(inputChar))
+            {
+                CheckInput(inputChar);
+            }
+        }
+    }
+
+    
+
+    public void CheckInput(string input) {
+        if (input == currentWord[0].ToString()) // Check if input matches first letter of currentWord
+        {
+            RemoveLetter();
+        }
+    }
+
+    public void RemoveLetter() {
+        currentWord = currentWord[1..]; // Remove the first letter
+        text.text = currentWord; // Update the word display
+
+        if (currentWord.Length == 0)
+        {
+            Timer.instance.TriggerWin();
+        }
+    }
+}
