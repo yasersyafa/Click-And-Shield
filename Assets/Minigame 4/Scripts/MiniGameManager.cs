6c829e864a94a468d01be23b683a6bf8d2e0c072
Louis Bima Susanto <69kamikazesusanto69@gmail.com>
2024-10-16 22:46:54 +0700

    Pause menu
    - Pause menu sudah jadi
    - Untuk time sudah jadi namun belum diaksih lose condition
    - Untuk object perlu sedikit revisi agar bisa berganti ganti prefab dalma 1 tipe

--- Assets/Minigame 4/Scripts/MiniGameManager.cs
+++ Assets/Minigame 4/Scripts/MiniGameManager.cs
@@ -6,12 +6,16 @@ using UnityEngine.SceneManagement;
 public class MiniGameManager : MonoBehaviour
 {
     public GameObject gameoverPanel;
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         gameoverPanel.SetActive(false);
+        pausePanel.SetActive(false);
     }
     
     public void GameOver()
@@ -26,5 +30,31 @@ public class MiniGameManager : MonoBehaviour
         Time.timeScale = 1f;
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PausedGame()
+    {
+        if (!isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (isPaused)
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Aplikasi keluar");
     }
 }
