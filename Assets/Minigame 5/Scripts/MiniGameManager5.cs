5e87d862e20261bad5b1cc4a3f084b6236607922
Louis Bima Susanto <69kamikazesusanto69@gmail.com>
2024-10-21 01:01:36 +0700

    Done
    - Object Spawner (Muncul Random)
    - Add Audio BGM
    - MiniGame Manager 5

---
+++ Assets/Minigame 5/Scripts/MiniGameManager5.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniGameManager5 : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isPaused)
+        {
+            Timer.instance.currentTimer -= Time.deltaTime;
+            if (Timer.instance.currentTimer <= 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    public void Win()
+    {
+        Debug.Log("Menang");
+        GameManager.instance.WinMinigame();
+        GameManager.instance.GoToRewardScene();
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Cupu");
+        GameManager.instance.LoseMinigame();
+        GameManager.instance.GoToRewardScene();
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PausedGame()
+    {
+        if (!isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (isPaused)
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
+
+    public void QuitGame()
+    {
+        GameManager.instance.QuitGame();
+    }
+}
