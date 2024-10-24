133e663363dbdfd578a528b2682ebfb98aec626b
dhityawe <awegamedev@gmail.com>
2024-10-24 12:33:08 +0700

    Update Feature: Refactor into using StatePattern

---
+++ Assets/Minigame 5/Scripts/WinState.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Video;
+using lindungiDataPribadi;
+
+namespace lindungiDataPribadi
+{
+    public class WinState : IMinigameState
+    {
+        private MinigameStateManager minigameManager;
+
+        public WinState(MinigameStateManager manager)
+        {
+            minigameManager = manager;
+        }
+
+        public void EnterState()
+        {
+            AudioManager.StopMusic();
+            minigameManager.cutsceneCanvas.SetActive(true);
+            minigameManager.cutscenePlayer.clip = minigameManager.winClip;
+            minigameManager.cutscenePlayer.Play();
+            minigameManager.cutscenePlayer.loopPointReached += EndWinCutscene;
+
+            // Call TriggerWin from GameManager and pass the win animation clip
+            // minigameManager.gameManager.TriggerWin(minigameManager.winClip);
+            GameManager.instance.WinMinigame();
+
+        }
+
+        public void UpdateState() { }
+
+        public void ExitState() { }
+
+        private void EndWinCutscene(VideoPlayer vp)
+        {
+            minigameManager.cutscenePlayer.loopPointReached -= EndWinCutscene;
+            Debug.Log("Win cutscene ended.");
+            GameManager.instance.GoToRewardScene();
+        }
+    }
+}
