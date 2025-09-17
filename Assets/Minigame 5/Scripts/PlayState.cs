ea9d91c79ffd4bfe1c7583b7dbde2871361afc4a
Yaser Syafa <syafayaser@gmail.com>
2025-09-17 11:05:15 +0700

    refactor(minigames): add safe area for mobile

--- Assets/Minigame 5/Scripts/PlayState.cs
+++ Assets/Minigame 5/Scripts/PlayState.cs
@@ -53,10 +53,21 @@ namespace lindungiDataPribadi
             for (int i = 0; i < trojanCount; i++)
             {
                 // Instantiate the trojanPrefab as a child of trojanParent with default position and rotation
-                GameObject trojan = GameObject.Instantiate(minigameManager.trojanPrefab, minigameManager.trojanParent.transform);
+                GameObject trojan = Object.Instantiate(minigameManager.trojanPrefab, minigameManager.trojanParent.transform);
+                RectTransform trojanRect = trojan.GetComponent<RectTransform>();
+
+                // calculate safe area
+                Rect safe = Screen.safeArea;
+
+                float xMin = safe.xMin - 50;
+                float xMax = safe.xMax - 50;
+
+                float yMin = safe.yMin - 50;
+                float yMax = safe.yMax - 50;
 
                 // Randomize position within the range (-3, 3) for x and (3, -3) for y
-                Vector2 randomPosition = new Vector2(Random.Range(-200f, 200f), Random.Range(100f, -50f));
+                Vector2 randomPosition = new(Random.Range(-100f, 100f), Random.Range(100f, -50f));
+                // Vector2 randomPos = new(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
 
                 // Set the position of the instantiated trojan
                 trojan.transform.localPosition = randomPosition; // Use localPosition since it's a child
